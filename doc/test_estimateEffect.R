1e548ec927871e9278bee03b4e6dcf0d5ff8f645
euphyw <euphyw@live.unc.edu>
2024-03-10 09:57:22 -0400

    estimate effectsize for simulation

--- doc/test_estimateEffect.R
+++ doc/test_estimateEffect.R
@@ -1,57 +1,29 @@
+setwd("/Users/Euphy/Desktop/Research/Single_Cell_Cancer/scLDAseq")
+library(splatter)
+library(scran)
+library(Rcpp)
+library(slam)
+library(SingleCellExperiment)
+library(Matrix)
+library(ggplot2)
+library(dplyr)
+library(mclust)
 
-params <- newSplatParams()
-params <- setParams(params, group.prob = c(0.5,0.3,0.2),
-                    de.prob = c(0.3, 0.3, 0.3), 
-                    nGenes = 600, batchCells=c(500,500,500),  
-                    lib.loc = 15)
-sims <- splatSimulate(params, method = "groups",
-                      verbose = FALSE, batch.rmEffect = FALSE)# create change of proportions
+sims <- readRDS("data/PositiveControl_2sample_3group_flip.rds")
 
-# we assume that the cells pre and post treatment are equal
-cell_count <- matrix(colSums(table(sims$Group, sims$Batch))/length(unique(sims$Group)))
-pre_prp <- matrix(c(0.5,0.2,0.3))
-pre_count <- cell_count %*% t(pre_prp)
-
-
-sampled_data <- colData(sims) %>%
-    data.frame() %>%
-    group_by(Group, Batch) %>%
-    mutate(time = case_when(
-        Group == "Group1" & Batch == "Batch1" & 
-            row_number() %in% sample(row_number(), min(pre_count[1,1], n())) ~ 1,
-        Group == "Group2" & Batch == "Batch1" & 
-            row_number() %in% sample(row_number(), min(pre_count[1,2], n())) ~ 1,
-        Group == "Group1" & Batch == "Batch2" & 
-            row_number() %in% sample(row_number(), min(pre_count[2,1], n())) ~ 1,
-        Group == "Group2" & Batch == "Batch2" & 
-            row_number() %in% sample(row_number(), min(pre_count[2,2], n())) ~ 1,
-        Group == "Group1" & Batch == "Batch3" & 
-            row_number() %in% sample(row_number(), min(pre_count[3,1], n())) ~ 1,
-        Group == "Group2" & Batch == "Batch3" & 
-            row_number() %in% sample(row_number(), min(pre_count[3,2], n())) ~ 1,
-        TRUE ~ 2 
-    )) %>%
-    ungroup() 
-
-
-time_prop <- sampled_data %>% 
+time_prop <- colData(sims) %>% 
     data.frame() %>%
     group_by(time, Batch) %>%
     count(Group, Batch, time) %>%
     mutate(Proportion = n / sum(n))
 
-
-sims$time <- sampled_data$time
-
-##### eval 
+# run multiSTM
 r.file <- paste0("R/",list.files("R/"))
-# r.file <- paste0("../stm/R/",list.files("../stm/R/"))
 sapply(r.file, source)
-# sourceCpp("../stm/src/STMCfuns.cpp")
 sourceCpp("src/STMCfuns.cpp")
 
 dat <- prepsce(sims)
-K <- 3
+K <- length(unique(sims$Group))
 prevalence <- as.formula(~dat$meta$time)
 content <- NULL
 sce <- dat$sce
@@ -60,7 +32,7 @@ vocab <- dat$vocab
 meta <- dat$meta
 sample <- "Batch"
 
-res <- multi_stm(documents = documents, vocab = vocab,
+res.multi <- multi_stm(documents = documents, vocab = vocab,
                  K = K, prevalence = prevalence, content = NULL,
                  data = meta, 
                  sce = sce,
@@ -70,9 +42,142 @@ res <- multi_stm(documents = documents, vocab = vocab,
                  kappa.prior= "L1",
                  control = list(gamma.maxits=3000))
 
+multi_eff <- estimateEffect(1:K ~ time, stmobj = res.multi, meta= meta, uncertainty = "Global")
+summary(multi_eff)
 
-# b_prep <-estimateEffect(1:5 ~ time + batch, stmobj = b, meta= meta,uncertainty = "Global")
+multi_eff_p1 <- estimateEffect(1:K ~ time, stmobj = res.multi, meta= meta, 
+                               sampleNames = "Batch", sampleIDs = "Batch1",uncertainty = "Global")
+summary(multi_eff_p1)
+plot(multi_eff_p1, "time", model=res.multi,
+     method="difference",cov.value1=1,cov.value2=2)
+png("res/estimate_effect_TwoGroupChange_patient1.png", width = 3500, height = 2500, res = 300)
+plot(multi_eff_p1, "time", xlim = c(-0.3,0.3),
+     main = "Estimated Effect of Time as a Covariate to Cell-Topic Proportion for Sample 1",
+     method="difference",cov.value1=1,cov.value2=2,
+     model = res.multi,
+     labeltype = "frex", 
+     xlab = "Proportion Difference (95% Confidence Interval) ")
+dev.off()
 
-form <- as.formula(1:5~time)
-form
+multi_eff_p2 <- estimateEffect(1:K ~ time, stmobj = res.multi, meta= meta, 
+                               sampleNames = "Batch", sampleIDs = "Batch2",uncertainty = "Global")
+summary(multi_eff_p2)
+png("res/estimate_effect_TwoGroupChange_patient2.png", width = 3500, height = 2500, res = 300)
+plot(multi_eff_p2, "time", xlim = c(-0.3,0.3),
+     main = "Estimated Effect of Time as a Covariate to Cell-Topic Proportion for Sample 2",
+     method="difference",cov.value1=1,cov.value2=2,
+     model = res.multi,
+     labeltype = "frex",
+     xlab = "Proportion Difference (95% Confidence Interval) ")
+dev.off()
 
+
+# plot the true proportion
+time_prop <- colData(sims) %>% 
+    data.frame() %>%
+    group_by(time, Batch) %>%
+    count(Group, Batch, time) %>%
+    mutate(Proportion = n / sum(n)) %>%
+    mutate(time = ifelse(time==1, "pre-treatment", "post-treatment")) %>%
+    mutate(Batch = gsub("Batch", "Sample", Batch)) %>%
+    mutate(Group = gsub("Group", "CellType_", Group))
+time_prop$time <- factor(time_prop$time, levels=c("pre-treatment", "post-treatment"))
+png("res/true_prop.png", height = 2500, width = 2500, res = 300)
+ggplot(time_prop, aes(x = Group, y = Proportion, fill = as.factor(time))) +
+    geom_bar(stat = "identity", position = "dodge") +
+    labs(x = "Group", y = "Proportion", fill = "time", 
+         title = "Cell Type True Proportion Change") +
+    theme_minimal() +
+    scale_fill_brewer(palette = "Set2") + 
+    facet_grid(~Batch) + 
+    theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust=0.2)) +
+    xlab(NULL)
+dev.off()
+
+
+# run starndard stm
+r.file <- paste0("../stm/R/",list.files("../stm/R/"))
+sapply(r.file, source)
+sourceCpp("../stm/src/STMCfuns.cpp")
+
+stm.res <- stm(documents = documents, vocab = vocab,
+                       K = K, prevalence = prevalence, content = NULL,
+                       data = meta, 
+                       sce = sce,
+                       init.type= "Spectral",
+                       gamma.prior= "Pooled",
+                       kappa.prior= "L1",
+                       control = list(gamma.maxits=3000))
+
+
+
+
+stm_eff <-estimateEffect(1:K ~ time, stmobj = stm.res, meta= meta,uncertainty = "Global")
+summary(stm_eff)
+
+#################################################
+################ positive control ################
+#################################################
+
+params <- newSplatParams()
+params <- setParams(params, group.prob = c(0.1,0.45,0.45),
+                    de.prob = c(0.2, 0.2, 0.2),
+                    nGenes = 500, batchCells=c(1000,1000))
+sims <- splatSimulate(params, method = "groups",
+                      verbose = FALSE, batch.rmEffect = FALSE)
+
+# we assume that the cells pre and post treatment are equal
+cell_count <- matrix(colSums(table(sims$Group, sims$Batch))/length(unique(sims$Group)))
+pre_prp <- matrix(c(0.1,0.65,0.25))
+pre_count <- cell_count %*% t(pre_prp)
+colnames(pre_count) <- paste0("Group",1:length(unique(sims$Group)))
+rownames(pre_count) <- paste0("Batch", 1:length(unique(sims$Batch)))
+
+sampled_data <- colData(sims) %>%
+    data.frame() %>%
+    group_by(Group, Batch) %>%
+    mutate(time = 2) %>%
+    ungroup()
+
+for (i in 1:nrow(pre_count)) {
+    batch_name <- rownames(pre_count)[i]
+    for (j in 1:ncol(pre_count)) {
+        group_name <- colnames(pre_count)[j]
+        sampled_data <- sampled_data %>%
+            group_by(Group, Batch) %>%
+            mutate(time = ifelse(
+                Group == group_name & Batch == batch_name &
+                    row_number() %in% sample(row_number(), min(pre_count[i,j], n())), 1, time)) %>%
+            ungroup()
+    }
+}
+sims$time <- sampled_data$time
+
+#### QC ######
+sims <- quickPerCellQC(sims)
+#### feature selection #####
+sims <- scuttle::logNormCounts(sims)
+dec.p2 <- modelGeneVar(sims)
+# feature selection
+p2.chosen <- getTopHVGs(dec.p2, n=2000)
+sims.sub <- sims[p2.chosen,]
+
+nsample <- length(unique(sims.sub$Batch))
+ngroup <- length(unique(sims.sub$Group))
+
+file_name <- paste0("data/PositiveControl_", nsample, "sample_",
+                    ngroup, "group.rds")
+
+saveRDS(sims.sub, file = file_name)
+
+
+# swap patient prop change
+rand.samp <- sample(unique(sims.sub$Batch), size = 1, replace = FALSE)
+sampled_data <- colData(sims.sub) %>%
+    data.frame() %>%
+    mutate(new_time = ifelse(time == 1 & Batch %in% rand.samp, 2, 1)) %>%
+    mutate(time = ifelse(Batch %in% rand.samp, new_time, time))
+sims.sub$time <- sampled_data$time
+file_name <- paste0("data/PositiveControl_", nsample, "sample_",
+                    ngroup, "group_flip.rds")
+saveRDS(sims.sub, file = file_name)
