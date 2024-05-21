18bfd5111ce868f3a8a6f409eb288eeab05765a2
FennecFish <euphy.w@gmail.com>
2024-05-20 23:31:13 -0400

    remove output

--- doc/CompositionChange/countsplit_scSTMseq.R
+++ doc/CompositionChange/countsplit_scSTMseq.R
@@ -35,7 +35,7 @@ sims <- sims[p2.chosen,]
 
 # batch effect removal using combat seq
 adjusted_counts <- ComBat_seq(counts(sims), batch=sims$Batch, group=NULL)
-counts(sims) <- adjusted_counts
+
 cat("Batch Effect Removed \n")
 
 ###### split count ############
@@ -43,5 +43,28 @@ split <- countsplit(adjusted_counts)
 Xtrain <- split[[1]]
 Xtest <- split[[2]]
 
+# save test dataset
 saveRDS(Xtest, file = paste0("/work/users/e/u/euphyw/scLDAseq/data/simulation/composition_change/V1/test/",
                              "test_", set_level, ".rds"))
+
+counts(sims) <- Xtrain
+nsample <- length(unique(sims$Batch))
+ngroup <- length(unique(sims$Group))
+
+r.file <- paste0("R/",list.files("R/"))
+sapply(r.file, source)
+sourceCpp("src/STMCfuns.cpp")
+
+t1 <- proc.time()
+
+scSTM.mod <- selectModel(sce = sims,
+                         K = ngroup, prevalence = ~time, content = NULL,
+                         sample = "Batch", N = 5, ts_runs = 20, random_run =20)
+
+all_values <- unlist(scSTM.mod$bound)
+max_value <- max(all_values)
+max_position_in_vector <- which(all_values == max_value)
+res <- scSTM.mod$runout[[max_position_in_vector]]
+msg <- sprintf("Completed scLDAseq (%d seconds). \n", floor((proc.time()-t1)[3]))
+saveRDS(res, file = paste0("/work/users/e/u/euphyw/scLDAseq/data/simulation/composition_change/V1/scSTM/",
+                           "scSTM_", set_level, ".rds"))
--- doc/CompositionChange/sims.sh
+++ doc/CompositionChange/sims.sh
@@ -5,8 +5,8 @@
 #SBATCH --error=sim_%A_%a.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=1:00:00
-#SBATCH --mem-per-cpu=20G
+#SBATCH --time=6:00:00
+#SBATCH --mem-per-cpu=15G
 #SBATCH --array=1
 #SBATCH --mail-type=all
 #SBATCH --mail-user=euphyw@live.unc.edu
