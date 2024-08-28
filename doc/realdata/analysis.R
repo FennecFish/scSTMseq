94555b7c34007ebe946aad9244f75a5686af23e4
euphyw <euphyw@live.unc.edu>
2024-08-27 22:11:36 -0400

    real data analysis

--- doc/realdata/analysis.R
+++ doc/realdata/analysis.R
@@ -1,7 +1,8 @@
 # Goal: This script is used to analyze scSTM from real data PD1
 # 1) compare assigned cluster from paper cluster
 # 2) changes between groups (time/expansion)
-setwd("/proj/milovelab/wu/scLDAseq")
+# setwd("/proj/milovelab/wu/scLDAseq")
+setwd("/Users/Euphy/Desktop/Research/Single_Cell_Cancer/scLDAseq")
 library(Matrix)
 library(SingleCellExperiment)
 library(ggplot2)
@@ -21,21 +22,38 @@ select_top_scSTM <- function(scSTMobj) {
   return(scSTMobj)
 }
 
-scSTMobj <- readRDS("/work/users/e/u/euphyw/scLDAseq/data/PD1_data/PD1_scSTM/scSTM_Content_TimeandResponse_Prevalence_TimeandSample.rds")
-
-dat <- select_top_scSTM(scSTMobj)
-sims <- dat$settings$sce
+# scSTMobj <- readRDS("/work/users/e/u/euphyw/scLDAseq/data/PD1_data/PD1_scSTM/scSTM_Content_TimeandResponse_Prevalence_TimeandSample.rds")
+scSTMobj <- readRDS("data/scSTM_Content_TimeandResponse_Prevalence_TimeandSample.rds")
+scSTMobj <- select_top_scSTM(scSTMobj)
+scSTMobj <- readRDS("data/test_interaction.rds")
+sims <- scSTMobj$settings$sce
 K <- length(unique(sims$cellType))
 
 r.file <- paste0("R/",list.files("R/"))
 sapply(r.file, source)
+structure_plot(scSTMobj, topics = 1:K, grouping = sims$cellType, n = 5000, gap = 5)
 time_effect <- estimateEffect(1:K ~ timepoint*expansion, 
-                              stmobj = dat, 
+                              stmobj = scSTMobj, ref <- c("Pre", "NE"),
                               sampleNames = "patient_id",
                               # sampleIDs = "BIOKEY_10",
                               uncertainty = "Global")
 
 summary(time_effect)
-plot.estimateEffect(time_effect, "Time", model=scSTM_nC_P,
-                    method="difference",cov.value1="Time1",cov.value2="Time2")
+plot.estimateEffect(time_effect, covariate = "timepoint", model=scSTMobj,
+                    method="difference",cov.value1="Pre",cov.value2="On")
+
+plot(time_effect, covariate = "timepoint", model = scSTMobj, 
+     #method = "difference",
+     method = "difference", cov.value1="Pre",cov.value2="On",
+     xlab = "timepoint", moderator = "expansion", 
+     moderator.value = "E", linecol = "blue", printlegend = FALSE) 
+
+plot(time_effect, covariate = "timepoint", model = scSTMobj, 
+     #method = "difference",
+     method = "difference", cov.value1="Pre",cov.value2="On",
+     xlab = "timepoint", moderator = "expansion", 
+     moderator.value = "NE", linecol = "red", add = T, 
+     printlegend = F) 
+legend(0, .08, c("Liberal", "Conservative"), + lwd = 2, col = c("blue", "red"))
+
 labelTopics(scSTM_nC_P, 4)
