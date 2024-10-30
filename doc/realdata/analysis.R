ca3455aed133d458f840196098326665d47d3c86
FennecFish <euphy.w@gmail.com>
2024-10-30 17:53:33 -0400

    merge

--- doc/realdata/analysis.R
+++ doc/realdata/analysis.R
@@ -7,6 +7,7 @@ library(Matrix)
 library(SingleCellExperiment)
 library(ggplot2)
 library(dplyr)
+library(mclust)
 
 select_top_scSTM <- function(scSTMobj) {
   if(class(scSTMobj) == "selectModel") {
@@ -22,25 +23,58 @@ select_top_scSTM <- function(scSTMobj) {
   return(scSTMobj)
 }
 
+cluster_scSTM <- function(scSTMobj) {
+    max_indices <- apply(scSTMobj$theta, 1, which.max)
+    colnames(scSTMobj$theta) <- paste0("topic_", 1:ncol(scSTMobj$theta))
+    rownames(scSTMobj$theta) <- scSTMobj$DocName
+    res_cluster <- colnames(scSTMobj$theta)[max_indices]
+    names(res_cluster) <- rownames(scSTMobj$theta)
+    return(res_cluster)
+}
+
 # scSTMobj <- readRDS("/work/users/e/u/euphyw/scLDAseq/data/PD1_data/PD1_scSTM/scSTM_Content_TimeandResponse_Prevalence_TimeandSample.rds")
-scSTMobj <- readRDS("data/scSTM_Content_TimeandResponse_Prevalence_TimeandSample.rds")
+scSTMobj <- readRDS("data/scSTM_Content_Batch_Prevalence_TimeResponseInteraction.rds")
 scSTMobj <- select_top_scSTM(scSTMobj)
-scSTMobj <- readRDS("data/test_interaction.rds")
 sims <- scSTMobj$settings$sce
+# calculate adjusted rand index
+scSTM_cluster <- cluster_scSTM(scSTMobj)
+scSTM_cluster <- scSTM_cluster[match(rownames(colData(sims)), names(scSTM_cluster))]
+adjustedRandIndex(scSTM_cluster, sims$cellType) # 0.6372073
+
 K <- length(unique(sims$cellType))
 
 r.file <- paste0("R/",list.files("R/"))
 sapply(r.file, source)
-structure_plot(scSTMobj, topics = 1:K, grouping = sims$cellType, n = 5000, gap = 5)
-time_effect <- estimateEffect(1:K ~ timepoint*expansion, 
-                              stmobj = scSTMobj, ref <- c("Pre", "NE"),
-                              sampleNames = "patient_id",
-                              # sampleIDs = "BIOKEY_10",
-                              uncertainty = "Global")
 
-summary(time_effect)
-plot.estimateEffect(time_effect, covariate = "timepoint", model=scSTMobj,
-                    method="difference",cov.value1="Pre",cov.value2="On")
+png("res/PD1/structure_plot_Interaction.png", height = 2000, width = 4000, res = 300)
+structure_plot(scSTMobj, topics = 1:K, grouping = sims$cellType, n = 3000, gap = 10)
+dev.off()
+
+# time effect
+time_effect_i <- estimateEffect(1:K ~ timepoint, 
+                              stmobj = scSTMobj, # ref = c("Pre", "NE"),
+                              sampleNames = "patient_id",
+                              sampleIDs = "BIOKEY_10",
+                              uncertainty = "Global")
+saveRDS(time_effect, file = "res/PD1/effect_Interaction.rds")
+
+time_effect_ni <- estimateEffect(1:K ~ timepoint, 
+                              stmobj = scSTMobj, # ref = c("Pre", "NE"),
+                              sampleNames = "patient_id",
+                              sampleIDs = "BIOKEY_10",
+                              uncertainty = "Global")
+# # try subset patients by expansion and non-expansion
+# # expansion
+# scSTMobj$settings$sce
+# time_effect <- estimateEffect(1:K ~ timepoint, 
+#                               stmobj = scSTMobj, # ref = c("Pre", "NE"),
+#                               ssampleNames = "patient_id",
+#                               sampleIDs = "BIOKEY_10",
+#                               uncertainty = "Global")
+# saveRDS(time_effect, file = "res/PD1/effect_Interaction.rds")
+# summary(time_effect)
+# plot.estimateEffect(time_effect, covariate = "timepoint", model=scSTMobj,
+#                     method="difference",cov.value1="Pre",cov.value2="On", linecol = "black")
 
 plot(time_effect, covariate = "timepoint", model = scSTMobj, 
      #method = "difference",
@@ -53,7 +87,7 @@ plot(time_effect, covariate = "timepoint", model = scSTMobj,
      method = "difference", cov.value1="Pre",cov.value2="On",
      xlab = "timepoint", moderator = "expansion", 
      moderator.value = "NE", linecol = "red", add = T, 
-     printlegend = F) 
-legend(0, .08, c("Liberal", "Conservative"), + lwd = 2, col = c("blue", "red"))
+     printlegend = T) 
+# legend( c("expansion", "non-expansion"), lwd = 2, col = c("blue", "red"))
 
 labelTopics(scSTM_nC_P, 4)
